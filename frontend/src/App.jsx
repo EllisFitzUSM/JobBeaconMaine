cc032cec4d93e3bc000004adc75b4fc86090f496
Kadin Ilott <kadin.ilott@maine.edu>
2025-12-04 15:53:13 -0500

    Got the website to launch and added AuthContext file to the folder to check if user is signed in before displaying recommended jobs button

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import JobResults from "./pages/JobResults.jsx";
+import SignInPage from "./pages/SignInPage.jsx";
+import PersonalizedSearchPage from "./pages/PersonalizedSearchPage.jsx";
+import "./styles/App.css";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/jobs" element={<JobResults />} />
+        <Route path="/personalized-search" element={<PersonalizedSearchPage />} />
+        <Route path="/signin" element={<SignInPage />} />
+      </Routes>
+    </Router>
+  );
+}
