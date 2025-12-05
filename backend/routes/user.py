3fc74a03c33230022ea063e57dd4646dd5ece6db
Jered10 <jered.kalombo@maine.edu>
2025-12-05 18:48:09 -0500

    Added new stored procedure + updated user routes

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, request, jsonify
 from db import get_db_connection
 
 user_routes = Blueprint('user_routes', __name__)
@@ -6,34 +6,44 @@ user_routes = Blueprint('user_routes', __name__)
 @user_routes.route('/user/<int:user_id>', methods=['GET', 'PUT'])
 def user_profile(user_id):
     conn = get_db_connection()
-    cursor = conn.cursor(dictionary=True)
+    cursor = conn.cursor()
 
     if request.method == 'GET':
-        cursor.execute("""
-            SELECT u.User_ID, u.First_Name, u.Last_Name, u.Email, u.City, 
-                   sa.Remote_Pref, sa.Salary_Min_Pref, sa.Salary_Max_Pref
-            FROM USER u
-            LEFT JOIN STUDENT_ALUM sa ON u.User_ID = sa.User_ID
-            WHERE u.User_ID = %s
-        """, (user_id,))
-        user = cursor.fetchone()
+        cursor.callproc('GetUserProfile', [user_id])
+        result = cursor.fetchall()
+
         cursor.close()
         conn.close()
-        return jsonify(user)
+        return jsonify(result[0]) if result else jsonify({})
 
     elif request.method == 'PUT':
         data = request.json
-        cursor.execute("""
-            UPDATE USER
-            SET First_Name=%s, Last_Name=%s, Email=%s, City=%s
-            WHERE User_ID=%s
-        """, (data['firstName'], data['lastName'], data['email'], data['city'], user_id))
-        cursor.execute("""
-            UPDATE STUDENT_ALUM
-            SET Remote_Pref=%s, Salary_Min_Pref=%s, Salary_Max_Pref=%s
-            WHERE User_ID=%s
-        """, (data['remotePref'], data['salaryMin'], data['salaryMax'], user_id))
+
+        cursor.callproc('UpdateUser', [
+            user_id,
+            data.get('firstName'),
+            data.get('lastName'),
+            data.get('middleInitial'),
+            data.get('email'),
+            data.get('phone'),
+            data.get('city'),
+            data.get('county'),
+            data.get('zip')
+        ])
+
+        cursor.callproc('UpdateStudentAlum', [
+            user_id,
+            data.get('distance'),
+            data.get('remotePref'),
+            data.get('culturePref'),
+            data.get('salaryMin'),
+            data.get('salaryMax'),
+            None,
+            None
+        ])
+
         conn.commit()
+
         cursor.close()
         conn.close()
-        return jsonify({'message': 'Profile updated successfully!'})
+        return jsonify({'message': 'Profile updated successfully'})
